63fa0256a79fc16589aa9d668e53781dcf096627
jaejunlee11 <sktks11@naver.com>
2024-05-19 21:53:49 +0900

    #11-리뷰 리슽 구현 완료

--- foohang/src/stores/review.js
+++ foohang/src/stores/review.js
@@ -4,10 +4,54 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useReviewStore = defineStore("review", () => {
-    const reviewList = ref([]);
+    const reviewList = ref([
+      {
+        title: 'Sample Title',
+        postDate: '2023-05-11',
+        travelDate: '2023-05-15',
+        images: [
+          '/src/assets/city/33-1.jpg',
+          '/src/assets/city/33-2.jpg',
+          '/src/assets/city/33-3.jpg',
+          '/src/assets/city/34-1.jpg',
+          '/src/assets/city/32-1.jpg',
+          '/src/assets/city/33-1.jpg'
+        ],
+        content: 'This is a sample post content.',
+        hashtags: ['#sample', '#post', '#content'],
+        selectedEmotion: '1'
+      },
+      {
+        title: 'Sample Title',
+        postDate: '2023-05-10',
+        travelDate: '2023-05-15',
+        images: [
+          '/src/assets/city/31-1.jpg',
+          '/src/assets/city/31-2.jpg',
+          '/src/assets/city/31-3.jpg',
+          '/src/assets/city/32-1.jpg',
+        ],
+        content: 'This is a sample post content.',
+        hashtags: ['#sample', '#post', '#content'],
+        selectedEmotion: '1'
+      },
+      {
+        title: 'Sample Title',
+        postDate: '2023-05-01',
+        travelDate: '2023-05-15',
+        images: [
+          '/src/assets/city/32-1.jpg',
+          '/src/assets/city/32-2.jpg',
+          '/src/assets/city/32-3.jpg',
+        ],
+        content: 'This is a sample post content.',
+        hashtags: ['#sample', '#post', '#content'],
+        selectedEmotion: '1'
+      }
+    ]);
     
     const postReview = async(formData) =>{
-        await mypageApi.post("/reviews", formData, {
+        await mypageApi.post("/reviews/", formData, {
             headers: {
               "Content-Type": "multipart/form-data",
             },
@@ -16,7 +60,7 @@ export const useReviewStore = defineStore("review", () => {
     }
 
     const getReviews = async() =>{
-        const response  = await mypageApi.get("/reviews")
+        const response  = await mypageApi.get("/reviews/")
         reviewList.value = response.data;
     }
 
