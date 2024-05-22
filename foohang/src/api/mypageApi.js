bee83337315b0e2153ab013ca3123bb2a9ad6ad7
jaejunlee17 <sktks11@naver.com>
2024-05-22 14:20:02 +0900

    fix: 마커 순서 표현

--- foohang/src/api/mypageApi.js
+++ foohang/src/api/mypageApi.js
@@ -5,30 +5,30 @@ const mypageApi = axios.create({
 });
 
 mypageApi.interceptors.request.use((config) => {
-    const authStore = useAuthStore();
-    const token = authStore.token;
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  
-    return config;
-  });
-  
+  const authStore = useAuthStore();
+  const token = authStore.token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 mypageApi.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      //권한 오류 발생 시
-      console.error("요청 응답 오류", error);
-      const status = error.response.status;
-      if (status == 401 && error.response.data == "Unauthorized") {
-        const authStore = useAuthStore();
-        authStore.logout();
-        alert("로그인이 필요합니다.");
-      } else if (status == 401) {
-        alert("권한이 없습니다.");
-      }
-      return Promise.reject(error); //원래 반환해야하는 형식 크게 고려하지 않아도 됨
+  (response) => response,
+  (error) => {
+    //권한 오류 발생 시
+    console.error("요청 응답 오류", error);
+    const status = error.response.status;
+    if (status == 401 && error.response.data == "Unauthorized") {
+      const authStore = useAuthStore();
+      authStore.logout();
+      alert("로그인이 필요합니다.!!!!");
+    } else if (status == 401) {
+      alert("권한이 없습니다.");
     }
-  );
-  
+    return Promise.reject(error); //원래 반환해야하는 형식 크게 고려하지 않아도 됨
+  }
+);
+
 export default mypageApi;
--- foohang/src/components/trail/MapView.vue
+++ foohang/src/components/trail/MapView.vue
@@ -10,7 +10,6 @@ import type { KakaoMapLatLngItem } from "vue3-kakao-maps";
 import { useAttractionStore } from "@/stores/attraction";
 const attractionStore = useAttractionStore();
 
-// 선택시 중심 좌표 이동을 위한 좌표 관리
 const props = defineProps({
   centerLat: Number,
   centerLong: Number,
@@ -33,7 +32,6 @@ const onLoadKakaoMapMarker = (newMarker: kakao.maps.Marker): void => {
   marker.value = newMarker;
 };
 
-// 선택된 attraction 마커 리스트 관리
 const restaurantList = ref(attractionStore.nearRestaurants);
 const around = async () => {
   await attractionStore.getNearRestaurants(props.centerContent);
@@ -49,7 +47,6 @@ const regist = async () => {
   emit("attractionEvent");
 };
 
-//주변 음식점 관리
 const visibleRefs = ref<boolean[]>([]);
 const markers = ref<kakao.maps.Marker[]>([]);
 const onClickKakaoMapMarkerR = (index: number): void => {
@@ -67,7 +64,6 @@ const registR = async (contentId) => {
   emit("attractionEvent");
 };
 
-//선택된 리스트 마커 및 직선 관리
 const latLngList: Ref<KakaoMapLatLngItem[]> = ref([]);
 const lineList: Ref<KakaoMapLatLngItem[]> = ref([]);
 console.log(props);
@@ -104,12 +100,31 @@ watch(latLngList, async () => {
   lineList.value = attractionStore.lineList;
   console.log(attractionStore.lineList);
 });
+
+const orderBottomMargin = ref<string>("35px");
+
+const getMarkerImageSrc = (order: number): string => {
+  // const colors = ["red", "blue", "green", "orange", "purple"];
+  const colors = ["#EE703F"];
+  const color = colors[order % colors.length];
+  return createSVGMarker(order, color);
+};
+
+const createSVGMarker = (order: number, color: string): string => {
+  const svg = `
+  <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+  <path d="M32 2C18.1 2 6 14.1 6 28c0 13 11.5 24.5 23.4 33.8 1.4 1.1 3.7 1.1 5.1 0C46.5 52.5 58 41 58 28 58 14.1 45.9 2 32 2z" fill="${color}" stroke="black" stroke-width="2"/>
+  <text x="32" y="36" font-size="22" font-family="Arial" fill="white" text-anchor="middle">${order}</text>
+</svg>
+
+  `;
+  return `data:image/svg+xml;base64,${btoa(svg)}`;
+};
 </script>
 
 <template>
   <KakaoMap :lat="centerLat" :lng="centerLong" width="100%" height="50rem">
     <div v-if="centerContent > 0">
-      <!-- 중심좌표 마커 -->
       <KakaoMapMarker
         :image="{
           imageSrc: '/src/assets/restaurantMarker.png',
@@ -143,7 +158,6 @@ watch(latLngList, async () => {
         </div>
       </KakaoMapInfoWindow>
 
-      <!-- 음식점 마커 -->
       <KakaoMapMarker
         v-for="(restaurant, index) in restaurantList"
         :key="restaurant.contentId"
@@ -184,13 +198,20 @@ watch(latLngList, async () => {
         </v-card-actions>
       </KakaoMapInfoWindow>
     </div>
-    <!-- 선택된 리스트 마커 -->
     <KakaoMapPolyline :latLngList="lineList" />
     <KakaoMapMarker
       v-for="(point, index) in latLngList"
       :key="index"
       :lat="point.lat"
       :lng="point.lng"
+      :image="{
+        imageSrc: getMarkerImageSrc(index + 1),
+        imageWidth: 40,
+        imageHeight: 40,
+        imageOption: {},
+      }"
+      :order-bottom-margin="orderBottomMargin"
+      class="select-marker"
     />
   </KakaoMap>
 </template>
@@ -203,5 +224,8 @@ watch(latLngList, async () => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+.slect-marker {
+  color: aliceblue;
 }
 </style>
